9c489e5cf02a17bce1804de39f65521bd0554ff7
tamilselvanps <mailtotamil16@gmail.com>
2024-12-30 08:20:09 +0530

    added all required files

---
+++ frontend/src/components/cart/ConfirmOrder.js
@@ -0,0 +1,130 @@
+import MetaData from '../layouts/MetaData';
+import { Fragment, useEffect } from 'react';
+import { validateShipping } from './Shipping';
+import { useSelector,useDispatch } from 'react-redux';
+import { Link, useNavigate } from 'react-router-dom';
+import CheckoutSteps from './CheckoutStep';
+import { orderCompleted } from "../../slices/cartSlice";
+import {createOrder} from '../../actions/orderActions'
+
+export default function ConfirmOrder () {
+    const { shippingInfo, items:cartItems } = useSelector(state => state.cartState);
+    const { user } = useSelector(state => state.authState);
+    const { error:orderError } = useSelector(state => state.orderState)
+    const navigate = useNavigate();
+    const dispatch = useDispatch()
+    const itemsPrice = cartItems.reduce((acc, item)=> (acc + item.price * item.quantity),0);
+    const shippingPrice = itemsPrice > 200 ? 0 : 25;
+    let taxPrice = Number(0.05 * itemsPrice);
+    const totalPrice = Number(itemsPrice + shippingPrice + taxPrice).toFixed(2);
+    taxPrice = Number(taxPrice).toFixed(2)
+    
+    // const processPayment = () => {
+    //     const data = {
+    //         itemsPrice,
+    //         shippingPrice,
+    //         taxPrice,
+    //         totalPrice
+    //     }
+    //     sessionStorage.setItem('orderInfo', JSON.stringify(data))
+    //     navigate('/payment')
+    // }
+    const submitOrder = () => {
+        const data = {
+            itemsPrice,
+            shippingPrice,
+            taxPrice,
+            totalPrice
+        }
+        sessionStorage.setItem('orderInfo', JSON.stringify(data))
+        const orderInfo = JSON.parse(sessionStorage.getItem('orderInfo'))
+        // navigate('/payment')
+        const order = {
+            orderItems: cartItems,
+            shippingInfo
+        }
+        if(orderInfo) {
+            order.itemsPrice = orderInfo.itemsPrice
+            order.shippingPrice = orderInfo.shippingPrice
+            order.taxPrice = orderInfo.taxPrice
+            order.totalPrice = orderInfo.totalPrice
+        }
+        dispatch(orderCompleted())
+        dispatch(createOrder(order))
+        navigate('/order/success')
+    }
+
+
+    useEffect(()=>{
+        validateShipping(shippingInfo, navigate)
+    },[])
+
+    return (
+        <Fragment>
+            <MetaData title={'Confirm Order'} />
+            <CheckoutSteps shipping confirmOrder />
+            <div className="row d-flex justify-content-between">
+            <div className="col-12 col-lg-8 mt-5 order-confirm">
+
+                <h4 className="mb-3">Shipping Info</h4>
+                <p><b>Name:</b> {user.name}</p>
+                <p><b>Phone:</b> {shippingInfo.phoneNo}</p>
+                <p className="mb-4"><b>Address:</b> {shippingInfo.address}, {shippingInfo.city}, {shippingInfo.postalCode}, {shippingInfo.state}, {shippingInfo.country} </p>
+                
+                <hr />
+                <h4 className="mt-4">Your Cart Items:</h4>
+
+                    {cartItems.map(item => (
+                            <Fragment>
+                                <div className="cart-item my-1">
+                                    <div className="row">
+                                        <div className="col-4 col-lg-2">
+                                            <img src={item.image} alt={item.name} height="45" width="65" />
+                                        </div>
+
+                                        <div className="col-5 col-lg-6">
+                                            <Link to={`/product/${item.product}`}>{item.name}</Link>
+                                        </div>
+
+
+                                        <div className="col-4 col-lg-4 mt-4 mt-lg-0">
+                                            <p>{item.quantity} x ${item.price} = <b>${item.quantity * item.price}</b></p>
+                                        </div>
+
+                                    </div>
+                                </div>
+                                <hr />
+                            </Fragment>
+                        )
+                    
+                        )
+                    
+                    }
+              
+              
+                
+
+            </div>
+			
+            <div className="col-12 col-lg-3 my-4">
+                    <div id="order_summary">
+                        <h4>Order Summary</h4>
+                        <hr />
+                        <p>Subtotal:  <span className="order-summary-values">${itemsPrice}</span></p>
+                        <p>Shipping: <span className="order-summary-values">${shippingPrice}</span></p>
+                        <p>Tax:  <span className="order-summary-values">${taxPrice}</span></p>
+
+                        <hr />
+
+                        <p>Total: <span className="order-summary-values">${totalPrice}</span></p>
+
+                        <hr />
+                        {/* <button id="checkout_btn" onClick={processPayment} className="btn btn-primary btn-block">Proceed to Payment</button> */}
+                        <button id="checkout_btn" onClick={submitOrder} className="btn btn-primary btn-block">Submit Order</button>
+                    </div>
+            </div>
+        </div>
+        </Fragment>
+        
+    )
+}
